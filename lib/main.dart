db70eb0857d28be9b056b3ab7b982b7f7813bdce
Gayrat <gayratshirinboyev6707@gmail.com>
2023-03-24 10:33:26 +0500

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 void main(){
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MyApp(),
-  ));
+  runApp(MyApp());
 }
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -15,59 +12,66 @@ class _MyAppState extends State<MyApp> {
   int scale = 0;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-      decoration: BoxDecoration(
-        image: DecorationImage(image: AssetImage('images/tasbeeh.png'))
-      ),
-      child: Scaffold(
-        appBar: AppBar(title: Center(child: Text('Tasbeeh')),backgroundColor: Colors.green,),
-        body: Column(mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Text('$scale',style: TextStyle(fontSize: 100),),
-            if(scale < 33 && scale >= 0)
-            Text('SubhanAllah',style: TextStyle(fontSize: 20)),
-            if(scale < 66 && scale >= 33)
-            Text('Alhamdulillah',style: TextStyle(fontSize: 20)),
-                  if(scale < 99 && scale >= 66 )
-            Text('Allohu Akbar',style: TextStyle(fontSize: 20)),
-             if(scale < 100 && scale >= 99 )
-            Text("""               La Ilaha Illalohu,
-              Wahdahula Sharika Lahu ,
-              Lahul-Mulku Wa Lahul-Hamdu,
-              Wa Huwa'AlaKulli  Sha'in Qadir
-              """,style: TextStyle(fontSize: 20)),
-           
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(title: Center(child: Text('Tasbeeh')),backgroundColor: Colors.green,),
+          body: Container(
+                  width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage('images/tasbeeh.png'),
+          fit: BoxFit.cover
+          ),
+          ),
+
+            child: Column(mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
+                Text('$scale',style: TextStyle(fontSize: 100,color: Colors.deepOrangeAccent),),
+                if(scale < 33 && scale >= 0)
+                Text('SubhanAllah',style: TextStyle(fontSize: 20,color: Colors.white)),
+                if(scale < 66 && scale >= 33)
+                Text('Alhamdulillah',style: TextStyle(fontSize: 20,color: Colors.white)),
+                      if(scale < 99 && scale >= 66 )
+                Text('Allohu Akbar',style: TextStyle(fontSize: 20,color: Colors.white)),
+                 if(scale < 100 && scale >= 99 )
+                Text("""               La Ilaha Illalohu,
+                  Wahdahula Sharika Lahu ,
+                  Lahul-Mulku Wa Lahul-Hamdu,
+                  Wa Huwa'AlaKulli  Sha'in Qadir
+                  """,style: TextStyle(fontSize: 20,color: Colors.white)),
+               
+                Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                         ElevatedButton(
+                      style: ElevatedButton.styleFrom(fixedSize: Size(100, 100),backgroundColor: Color(0xFF8179DD)),
+                      onPressed: (){
+                      setState(() {
+                        if(scale != 0){
+                          scale -=1;
+                        }
+                      });
+                    }, child: Text('—',style: TextStyle(fontSize: 50),)),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(fixedSize: Size(100, 100),backgroundColor: Colors.green),
+                      onPressed: (){
+                      setState(() {
+                        scale +=1;
+                      });
+                    }, child: Icon(Icons.add,size: 50,)),
                      ElevatedButton(
-                  style: ElevatedButton.styleFrom(fixedSize: Size(100, 100),backgroundColor: Colors.grey),
-                  onPressed: (){
-                  setState(() {
-                    if(scale != 0){
-                      scale -=1;
-                    }
-                  });
-                }, child: Text('—',style: TextStyle(fontSize: 50),)),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(fixedSize: Size(100, 100),backgroundColor: Colors.green),
-                  onPressed: (){
-                  setState(() {
-                    scale +=1;
-                  });
-                }, child: Icon(Icons.add,size: 50,)),
-                 ElevatedButton(
-                  style: ElevatedButton.styleFrom(fixedSize: Size(100,100),backgroundColor: Colors.red),
-                  onPressed: (){
-                  setState(() {
-                    scale = 0;
-                  });
-                }, child: Text('reset',style: TextStyle(fontSize: 25))),
+                      style: ElevatedButton.styleFrom(fixedSize: Size(100,100),backgroundColor: Colors.red),
+                      onPressed: (){
+                      setState(() {
+                        scale = 0;
+                      });
+                    }, child: Text('reset',style: TextStyle(fontSize: 25))),
+                  ],
+                )
               ],
-            )
-          ],
+            ),
+          ),
         ),
-      ),
     );
   }
 }
