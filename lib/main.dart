e11b9e9045966c634c94a86c05d19518dbe2dcc5
Gayrat <gayratshirinboyev6707@gmail.com>
2023-03-24 10:37:48 +0500

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -27,13 +27,13 @@ class _MyAppState extends State<MyApp> {
 
             child: Column(mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Text('$scale',style: TextStyle(fontSize: 100,color: Colors.deepOrangeAccent),),
+                Text('$scale',style: TextStyle(fontSize: 120,color: Colors.white),),
                 if(scale < 33 && scale >= 0)
-                Text('SubhanAllah',style: TextStyle(fontSize: 20,color: Colors.white)),
+                Text('SubhanAllah',style: TextStyle(fontSize: 30,color: Colors.white)),
                 if(scale < 66 && scale >= 33)
-                Text('Alhamdulillah',style: TextStyle(fontSize: 20,color: Colors.white)),
+                Text('Alhamdulillah',style: TextStyle(fontSize: 30,color: Colors.white)),
                       if(scale < 99 && scale >= 66 )
-                Text('Allohu Akbar',style: TextStyle(fontSize: 20,color: Colors.white)),
+                Text('Allohu Akbar',style: TextStyle(fontSize: 30,color: Colors.white)),
                  if(scale < 100 && scale >= 99 )
                 Text("""               La Ilaha Illalohu,
                   Wahdahula Sharika Lahu ,
@@ -58,7 +58,7 @@ class _MyAppState extends State<MyApp> {
                       setState(() {
                         scale +=1;
                       });
-                    }, child: Icon(Icons.add,size: 50,)),
+                    }, child: Text('+',style: TextStyle(fontSize: 50),)),
                      ElevatedButton(
                       style: ElevatedButton.styleFrom(fixedSize: Size(100,100),backgroundColor: Colors.red),
                       onPressed: (){
